d7d027df35a57801bba97ac4919941a63e1a71ed
Nelson Durrant <snelsondurrant@gmail.com>
2024-10-18 09:27:31 -0600

    Quick fix

--- ros1_bridge/sync_msgs.sh
+++ ros1_bridge/sync_msgs.sh
@@ -3,4 +3,4 @@
 #
 # Syncs the messages in ros2_msgs_ws with the messages in the general ROS2 repository
 
-rsync -avphd ~/Autonomy-ROS2/mars_ws/src/rover_msgs ~/Autonomy-ROS2/bridge/ros2_msgs_ws/src
+rsync -avphd ~/Autonomy-ROS2/mars_ws/src/rover_msgs ~/Autonomy-ROS2/ros1_bridge/ros2_msgs_ws/src
