88e4c0bec349d90e73995b225799886a10a4f5d4
Daniel Webb <dbw2002@byu.edu>
2024-10-25 10:15:33 -0600

    FEAT: mapviz_tf builds now, I changed the name of the ROS1 package because you can't have two packages with the same name.

--- mars_ws/src/mapviz_tf-ROS1/setup.py
+++ mars_ws/src/mapviz_tf-ROS1/setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
-package_name = 'mapviz_tf'
+package_name = 'mapviz_tf_ros1'
 
 setup(
     name= package_name,
     version="0.0.0",
     install_requires=['setuptools'],
     zip_safe=True,
-    packages = ['mapviz_tf'],
+    packages = ['mapviz_tf_ros1'],
     package_dir={'': 'src'},
     maintainer='Michael Crandall',
     maintainer_email="wyomike2020@gmail.com",
--- mars_ws/src/mapviz_tf/setup.py
+++ mars_ws/src/mapviz_tf/setup.py
@@ -1,30 +1,29 @@
-from distutils.core import setup
+from setuptools import find_packages, setup
 
 package_name = 'mapviz_tf'
 
 setup(
     name= package_name,
     version="0.0.0",
+    packages =find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
-    packages = ['mapviz_tf'],
-    package_dir={'': 'src'},
     maintainer='Michael Crandall',
     maintainer_email="wyomike2020@gmail.com",
     description='The MapViz package',
-    license='TODO',
-
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name])
-        ('share/' + package_name, ['package.xml'])
-    ],
+    license='Apache-2.0',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'click_waypoint = click_waypoint:main'  #Figure out what we'll put in here instead of the node name
             'gps_to_mapviz = gps_to_mapviz:main'
             'path_to_mapviz = path_to_mapviz:main'
             'rover_tf_broadcast = rover_tf_broadcast:main'
-        ]
-    }
+        ],
+    },
 )
