38dddd87fbfd0b36d1dc2142a67e16397603bf42
Nelson Durrant <snelsondurrant@gmail.com>
2024-11-06 08:56:29 -0700

    Documentation fixes

--- ros1_bridge/scripts/run_bridge.sh
+++ ros1_bridge/scripts/run_bridge.sh
@@ -3,6 +3,10 @@
 #
 # Runs the ROS 1 bridge
 
+# Run 'echo $ROS_MASTER_URI' from a sourced, running ROS 1 terminal 
+# from outside the Docker image to get the IP and/or port address to add here
+export ROS_MASTER_URI=http://192.168.1.120:11311
+
 source /opt/ros/noetic/setup.bash
 source ~/ros1_msgs_ws/install_isolated/setup.bash
 source ~/ros2_humble/install/setup.bash
