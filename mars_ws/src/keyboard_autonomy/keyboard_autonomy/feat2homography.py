2542ca1d979acd0d3d42be471c33b76598da904c
Nelson Durrant <snelsondurrant@gmail.com>
2024-11-08 09:43:18 -0700

    feat: added opencv code to ros node

--- mars_ws/src/keyboard_autonomy/keyboard_autonomy/feat2homography.py
+++ mars_ws/src/keyboard_autonomy/keyboard_autonomy/feat2homography.py
@@ -2,8 +2,11 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+import numpy as np
 import cv2
 from rover_msgs.msg import KeyboardHomography
+
+MIN_MATCH_COUNT = 10
 
 class Feat2HomographyNode(Node):
     '''
@@ -39,7 +42,7 @@ class Feat2HomographyNode(Node):
         '''
 
         self.bridge = CvBridge()
-        keyboard_img = cv2.imread("../images/keyboard_cropped.jpg")
+        self.keyboard_img = cv2.imread("../images/keyboard_cropped.jpg")
 
     def listener_callback(self, msg):
         '''
@@ -52,12 +55,44 @@ class Feat2HomographyNode(Node):
         # Convert ROS Image to OpenCV
         camera_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
 
-        # TODO: Add SIFT algorithm to find keypoints and descriptors in both images
-        # TODO: Add FLANN algorithm to find matches between the descriptors
-        # TODO: Add homography calculation (Should be a 3x3 matrix)
+        # Initiate SIFT detector and find the keypoints and descriptors
+        sift = cv2.SIFT_create(nfeatures=500, nOctaveLayers=2, contrastThreshold=0.04, edgeThreshold=10)
+        kp1, des1 = sift.detectAndCompute(self.keyboard_image, None)
+        kp2, des2 = sift.detectAndCompute(camera_image, None)
+
+        # Set FLANN parameters
+        FLANN_INDEX_KDTREE = 0
+        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        search_params = dict(checks = 50)
+
+        # Create the FLANN matcher and find matches (best two matches for each descriptor)
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+        matches = flann.knnMatch(des1, des2, k=2)
+
+        # Store all the good matches using Lowe's ratio test
+        # Check to make sure that the distance to the closest match is sufficiently
+        # less than the distance to the second closest match
+        good_matches = []
+        for m, n in matches:
+            if m.distance < 0.7 * n.distance:
+                good_matches.append(m)
+
+        # If enough matches are found, calculate the homography
+        if len(good_matches) >= MIN_MATCH_COUNT:
+
+            # Get the keypoints from the good matches
+            src_pts = np.float32([ kp1[m.queryIdx].pt for m in good_matches ]).reshape(-1, 1, 2)
+            dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good_matches ]).reshape(-1, 1, 2)
+
+            # Calculate the homography matrix using RANSAC
+            M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+
+        else:
+            self.get_logger().warn("Insufficient # of matches found - %d/%d" % (len(good_matches), MIN_MATCH_COUNT))
 
         keyboard_homography = KeyboardHomography()
-        keyboard_homography.homography = None
+        keyboard_homography.header.stamp = msg.header.stamp
+        keyboard_homography.homography = M.flatten().tolist() # TODO: Test this
 
         self.publisher_.publish(keyboard_homography)
 
