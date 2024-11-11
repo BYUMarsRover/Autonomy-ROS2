1e4de154801df52474e522269b9cc36af412d83f
Nelson Durrant <snelsondurrant@gmail.com>
2024-11-11 09:49:25 -0700

    fix: quick fix

--- mars_ws/src/keyboard_autonomy/launch/keyboard_autonomy_launch.py
+++ mars_ws/src/keyboard_autonomy/launch/keyboard_autonomy_launch.py
@@ -11,7 +11,7 @@ def generate_launch_description():
     Launches the keyboard autonomy nodes.
 
     IMPORTANT!: Use this format when calling the launch file:
-    'ros2 launch keyboard_autonomy keyboard_autonomy_launch.py word:=hello'
+    'ros2 launch keyboard_autonomy keyboard_autonomy_launch.py word:=hello' # TODO: Test this
 
     :return: The launch description.
     '''
