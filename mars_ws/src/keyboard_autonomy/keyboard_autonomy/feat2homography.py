239b15f5c6c08afe132feab7cfd77e47362c442d
chlohilt <hilton.chloe.y@gmail.com>
2024-11-15 09:06:14 -0700

    feat: get new orb functions set up in node

--- mars_ws/src/keyboard_autonomy/keyboard_autonomy/feat2homography.py
+++ mars_ws/src/keyboard_autonomy/keyboard_autonomy/feat2homography.py
@@ -1,4 +1,5 @@
 import rclpy
+from matplotlib import pyplot as plt
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
@@ -37,7 +38,7 @@ class Feat2HomographyNode(Node):
         '''
         self.subscription  # Prevent unused variable warning
 
-        self.publisher_ = self.create_publisher(KeyboardHomography, '/keyboard_homography', 10)
+        self.publisher = self.create_publisher(KeyboardHomography, '/keyboard_homography', 10)
         '''
         Publisher to the "/keyboard_homography" topic with the message type KeyboardHomography.
         '''
@@ -47,25 +48,53 @@ class Feat2HomographyNode(Node):
         # self.keyboard_img = cv2.imread("../images/keyboard_cropped.jpg")
         start_time = time.time()
 
-        # Load the images using OpenCV
-        img1 = cv2.imread("/home/marsrover/mars_ws/src/keyboard_autonomy/images/keyboard_better.jpg")
-        img2 = cv2.imread("/home/marsrover/mars_ws/src/keyboard_autonomy/images/keyboard1.jpg")
+        elapsed_100 = self.orb_calculate("/home/marsrover/mars_ws/src/keyboard_autonomy/images/keyboard_better_rotated.jpg", "/home/marsrover/mars_ws/src/keyboard_autonomy/images/keyboard_better.jpg",
+                                    'key_board_better_inverted_compare.png', orb_create_matches=100)
+        elapsed_2000 = self.orb_calculate("/home/marsrover/mars_ws/src/keyboard_autonomy/images/keyboard_better_rotated.jpg", "/home/marsrover/mars_ws/src/keyboard_autonomy/images/keyboard_better.jpg",
+                                     'key_board_better_inverted_compare.png', orb_create_matches=2000)
+        elapsed_4000 = self.orb_calculate("/home/marsrover/mars_ws/src/keyboard_autonomy/images/keyboard_better_rotated.jpg", "/home/marsrover/mars_ws/src/keyboard_autonomy/images/keyboard_better.jpg",
+                                     'key_board_better_inverted_compare.png', orb_create_matches=4000)
+        elapsed_6000 = self.orb_calculate("/home/marsrover/mars_ws/src/keyboard_autonomy/images/keyboard_better_rotated.jpg", "/home/marsrover/mars_ws/src/keyboard_autonomy/images/keyboard_better.jpg",
+                                     'key_board_better_inverted_compare.png', orb_create_matches=6000)
+        elapsed_8000 = self.orb_calculate("/home/marsrover/mars_ws/src/keyboard_autonomy/keyboard_better_rotated.jpg", "/home/marsrover/mars_ws/src/keyboard_autonomy/images/keyboard_better.jpg",
+                                     'key_board_better_inverted_compare.png', orb_create_matches=8000)
 
+
+    def listener_callback(self, msg):
+        '''
+        Callback function for the "TODO: Add here" subscription.
+        Uses the SIFT and FLANN algorithms to find the homography between the keyboard image and the
+        received image.
+
+        :param msg: The Image message received from the "TODO: Add here" topic.
+        '''
+        # Convert ROS Image to OpenCV
+        camera_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        elapsed_100 = self.orb_calculate("../images/keyboard_better_rotated.jpg", camera_image,
+                                    'key_board_better_inverted_compare.png', orb_create_matches=500)
+
+    def sift_calculate(self, first_image, second_image, path_to_save_result, orb_create_matches=300, min_match_count=10,
+                       flann_index_lsh=6):
+        start_time = time.time()
+
+        # Minimum number of matches required
+        MIN_MATCH_COUNT = min_match_count
+
+        # Load the images using OpenCV
+        img1 = cv2.imread(first_image)
+        img2 = cv2.imread(second_image)
         image_read_time = time.time()
+
+        # Initiate SIFT detector and find the keypoints and descriptors
+        sift = cv2.SIFT_create(nfeatures=500, nOctaveLayers=2, contrastThreshold=0.04, edgeThreshold=10)
+        kp1, des1 = sift.detectAndCompute(img1, None)
+        kp2, des2 = sift.detectAndCompute(img2, None)
         sift_detect_and_compute_time = time.time()
 
-        orb = cv2.ORB_create(600)
-        kp1, des1 = orb.detectAndCompute(img1, None)
-        kp2, des2 = orb.detectAndCompute(img2, None)
-        orb_detect_and_compute_time = time.time()
-
-        # Set FLANN parameters (TODO: What are these?)
+        # Set FLANN parameters
         FLANN_INDEX_KDTREE = 0
-        FLANN_INDEX_LSH = 6
-        index_params = dict(algorithm=FLANN_INDEX_LSH, table_number=6)  # key_size = 12, multi_probe_level = 1) #2
-        # index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
         search_params = dict(checks=50)
-
         parameter_dictionary_time = time.time()
 
         # Create the FLANN matcher and find matches (best two matches for each descriptor)
@@ -78,19 +107,14 @@ class Feat2HomographyNode(Node):
         # Check to make sure that the distance to the closest match is sufficiently
         # less than the distance to the second-closest match
         good_matches = []
-        skipped_matches = 0
-        for match_pair in matches:
-            if len(match_pair) == 2:  # Ensure we have two matches
-                m, n = match_pair
-                if m.distance < 0.7 * n.distance:
-                    good_matches.append(m)
-            else:
-                skipped_matches += 1
+        for m, n in matches:
+            if m.distance < 0.7 * n.distance:
+                good_matches.append(m)
 
         # If enough matches are found, calculate the homography
+        homography_calculated_time = 0
         if len(good_matches) >= MIN_MATCH_COUNT:
             print(f'Found: {len(good_matches)} good matches out of {len(matches)} total matches')
-            print(f'skipped: {skipped_matches} because there weren\'t two pairs')
 
             # Get the keypoints from the good matches
             src_pts = np.float32([kp1[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
@@ -103,64 +127,63 @@ class Feat2HomographyNode(Node):
             homography_calculated_time = time.time()
             # Plot and display the results
             h, w = img1.shape[0:2]
-            keyboard_homography = KeyboardHomography()
-            keyboard_homography.homography = M.flatten().tolist()  # TODO: Test this
-            self.get_logger().info("Keyboard homography")
-            self.get_logger().info(str(keyboard_homography))
-            self.publisher_.publish(keyboard_homography)
+            pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)  # get the corners
+            outline = cv2.perspectiveTransform(pts, M)  # transform the corners of the first image onto the second
+            img2 = cv2.polylines(img2, [np.int32(outline)], True, 255, 20, cv2.LINE_AA)  # draw the box
+            draw_params = dict(
+                singlePointColor=None,
+                matchesMask=matchesMask,
+                flags=2,
+                matchesThickness=20  # Adjust this value to change the line thickness between matched points
+            )
+            img3 = cv2.drawMatches(img1, kp1, img2, kp2, good_matches, None, **draw_params)
+            plt.imshow(img3, 'gray')
+            plt.savefig(path_to_save_result)
+            # plt.show()
 
         else:
             print("Not enough matches are found - %d/%d" % (len(good_matches), MIN_MATCH_COUNT))
 
+        end_time = time.time()
+
+        elapsed_time = end_time - start_time
+
         print(f"time for image reading: {image_read_time - start_time}")
         print(
             f"time for detecting and computing SIFT keypoints and descriptors: {sift_detect_and_compute_time - image_read_time}")
-        print(
-            f"time for detecting and computing ORB keypoints and descriptors: {orb_detect_and_compute_time - sift_detect_and_compute_time}")
-        print(f"time for creating the dictionaries: {parameter_dictionary_time - orb_detect_and_compute_time}")
+        print(f"time for creating the dictionaries: {parameter_dictionary_time - sift_detect_and_compute_time}")
         print(f"time for calling FlannBasedMatcher and knnMatch: {matches_calculated_time - parameter_dictionary_time}")
         print(
             f"time for calculating the homography matrix and mask: {homography_calculated_time - matches_calculated_time}")
 
 
-    def listener_callback(self, msg):
-        '''
-        Callback function for the "TODO: Add here" subscription.
-        Uses the SIFT and FLANN algorithms to find the homography between the keyboard image and the
-        received image.
-
-        :param msg: The Image message received from the "TODO: Add here" topic.
-        '''
-        # Convert ROS Image to OpenCV
-        #camera_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-
+    def orb_calculate(self, first_image, second_image, path_to_save_result, orb_create_matches=300, min_match_count=10,
+                      flann_index_lsh=6):
         start_time = time.time()
 
+        # Minimum number of matches required
+        MIN_MATCH_COUNT = min_match_count
+
         # Load the images using OpenCV
-        img1 = cv2.imread("../images/keyboard_better.jpg")
-        img2 = cv2.imread("../images/keyboard1.jpg")
-
+        img1 = cv2.imread(first_image)
+        img2 = cv2.imread(second_image)
         image_read_time = time.time()
-        sift_detect_and_compute_time = time.time()
 
-        orb = cv2.ORB_create(600)
+        # Do the orb create and detect
+        orb = cv2.ORB_create(orb_create_matches)
         kp1, des1 = orb.detectAndCompute(img1, None)
         kp2, des2 = orb.detectAndCompute(img2, None)
         orb_detect_and_compute_time = time.time()
 
-        # Set FLANN parameters (TODO: What are these?)
-        FLANN_INDEX_KDTREE = 0
-        FLANN_INDEX_LSH = 6
-        index_params = dict(algorithm=FLANN_INDEX_LSH, table_number=6)  # key_size = 12, multi_probe_level = 1) #2
-        # index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        # Set FLANN parameters
+        FLANN_INDEX_LSH = flann_index_lsh
+        index_params = dict(algorithm=FLANN_INDEX_LSH, table_number=6)
         search_params = dict(checks=50)
-
         parameter_dictionary_time = time.time()
 
         # Create the FLANN matcher and find matches (best two matches for each descriptor)
         flann = cv2.FlannBasedMatcher(index_params, search_params)
         matches = flann.knnMatch(des1, des2, k=2)
-
         matches_calculated_time = time.time()
 
         # Store all the good matches using Lowe's ratio test
@@ -176,6 +199,7 @@ class Feat2HomographyNode(Node):
             else:
                 skipped_matches += 1
 
+        homography_calculated_time = 0
         # If enough matches are found, calculate the homography
         if len(good_matches) >= MIN_MATCH_COUNT:
             print(f'Found: {len(good_matches)} good matches out of {len(matches)} total matches')
@@ -192,25 +216,36 @@ class Feat2HomographyNode(Node):
             homography_calculated_time = time.time()
             # Plot and display the results
             h, w = img1.shape[0:2]
-            keyboard_homography = KeyboardHomography()
-            keyboard_homography.header.stamp = msg.header.stamp
-            keyboard_homography.homography = M.flatten().tolist()  # TODO: Test this
-            self.get_logger().info("Keyboard homography")
-            self.get_logger().info(str(keyboard_homography))
-            self.publisher_.publish(keyboard_homography)
+            pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)  # get the corners
+            outline = cv2.perspectiveTransform(pts, M)  # transform the corners of the first image onto the second
+            img2 = cv2.polylines(img2, [np.int32(outline)], True, 255, 20, cv2.LINE_AA)  # draw the box
+            draw_params = dict(
+                singlePointColor=None,
+                matchesMask=matchesMask,
+                flags=2,
+                matchesThickness=20  # Adjust this value to change the line thickness between matched points
+            )
+            img3 = cv2.drawMatches(img1, kp1, img2, kp2, good_matches, None, **draw_params)
+            plt.imshow(img3, 'gray')
+            plt.savefig(path_to_save_result)
+            # showing the plot currently doesn't work
+            # plt.show()
 
         else:
             print("Not enough matches are found - %d/%d" % (len(good_matches), MIN_MATCH_COUNT))
 
+        end_time = time.time()
+        elapsed_time = end_time - start_time
         print(f"time for image reading: {image_read_time - start_time}")
         print(
-            f"time for detecting and computing SIFT keypoints and descriptors: {sift_detect_and_compute_time - image_read_time}")
-        print(
-            f"time for detecting and computing ORB keypoints and descriptors: {orb_detect_and_compute_time - sift_detect_and_compute_time}")
+            f"time for detecting and computing ORB keypoints and descriptors: {orb_detect_and_compute_time - image_read_time}")
         print(f"time for creating the dictionaries: {parameter_dictionary_time - orb_detect_and_compute_time}")
         print(f"time for calling FlannBasedMatcher and knnMatch: {matches_calculated_time - parameter_dictionary_time}")
         print(
             f"time for calculating the homography matrix and mask: {homography_calculated_time - matches_calculated_time}")
+        keyboard_homography = KeyboardHomography()
+        keyboard_homography.homography = M.flatten().tolist()
+        self.publisher.publish(keyboard_homography)
 
 
 def main(args=None):
--- mars_ws/src/keyboard_autonomy/scripts/test_homography.py
+++ mars_ws/src/keyboard_autonomy/scripts/test_homography.py
@@ -13,7 +13,9 @@ and then uses the FLANN algorithm to find matches between the descriptors.
 If enough matches are found, the homography is calculated and displayed.
 '''
 
-def sift_calculate(first_image,second_image,path_to_save_result,orb_create_matches=300,min_match_count = 10, flann_index_lsh= 6):
+
+def sift_calculate(first_image, second_image, path_to_save_result, orb_create_matches=300, min_match_count=10,
+                   flann_index_lsh=6):
     start_time = time.time()
 
     # Minimum number of matches required
@@ -32,8 +34,8 @@ def sift_calculate(first_image,second_image,path_to_save_result,orb_create_match
 
     # Set FLANN parameters
     FLANN_INDEX_KDTREE = 0
-    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-    search_params = dict(checks = 50)
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)
     parameter_dictionary_time = time.time()
 
     # Create the FLANN matcher and find matches (best two matches for each descriptor)
@@ -44,21 +46,19 @@ def sift_calculate(first_image,second_image,path_to_save_result,orb_create_match
 
     # Store all the good matches using Lowe's ratio test
     # Check to make sure that the distance to the closest match is sufficiently
-    # less than the distance to the second closest match
+    # less than the distance to the second-closest match
     good_matches = []
     for m, n in matches:
         if m.distance < 0.7 * n.distance:
             good_matches.append(m)
 
-
-
     # If enough matches are found, calculate the homography
     if len(good_matches) >= MIN_MATCH_COUNT:
-        print(f'Found: {len(good_matches)} good matches out of {len(matches)} total matches')        
+        print(f'Found: {len(good_matches)} good matches out of {len(matches)} total matches')
 
         # Get the keypoints from the good matches
-        src_pts = np.float32([ kp1[m.queryIdx].pt for m in good_matches ]).reshape(-1, 1, 2)
-        dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good_matches ]).reshape(-1, 1, 2)
+        src_pts = np.float32([kp1[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+        dst_pts = np.float32([kp2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
 
         # Calculate the homography matrix and mask
         M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
@@ -67,15 +67,15 @@ def sift_calculate(first_image,second_image,path_to_save_result,orb_create_match
         homography_calculated_time = time.time()
         # Plot and display the results
         h, w = img1.shape[0:2]
-        pts = np.float32([[0, 0],[0, h-1],[w-1, h-1],[w-1, 0]]).reshape(-1, 1, 2) # get the corners
-        outline = cv2.perspectiveTransform(pts, M) # transform the corners of the first image onto the second
-        img2 = cv2.polylines(img2, [np.int32(outline)], True, 255, 20, cv2.LINE_AA) # draw the box
+        pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)  # get the corners
+        outline = cv2.perspectiveTransform(pts, M)  # transform the corners of the first image onto the second
+        img2 = cv2.polylines(img2, [np.int32(outline)], True, 255, 20, cv2.LINE_AA)  # draw the box
         draw_params = dict(
-                singlePointColor = None,
-                matchesMask = matchesMask,
-                flags = 2,
-                matchesThickness=20      # Adjust this value to change the line thickness between matched points
-                )
+            singlePointColor=None,
+            matchesMask=matchesMask,
+            flags=2,
+            matchesThickness=20  # Adjust this value to change the line thickness between matched points
+        )
         img3 = cv2.drawMatches(img1, kp1, img2, kp2, good_matches, None, **draw_params)
         plt.imshow(img3, 'gray')
         plt.savefig(path_to_save_result)
@@ -88,14 +88,17 @@ def sift_calculate(first_image,second_image,path_to_save_result,orb_create_match
 
     elapsed_time = end_time - start_time
 
-    print(f"time for image reading: {image_read_time-start_time}")
-    print(f"time for detecting and computing SIFT keypoints and descriptors: {sift_detect_and_compute_time-image_read_time}")
-    print(f"time for creating the dictionaries: {parameter_dictionary_time-sift_detect_and_compute_time}")
-    print(f"time for calling FlannBasedMatcher and knnMatch: {matches_calculated_time-parameter_dictionary_time}")
-    print(f"time for calculating the homography matrix and mask: {homography_calculated_time-matches_calculated_time}")
+    print(f"time for image reading: {image_read_time - start_time}")
+    print(
+        f"time for detecting and computing SIFT keypoints and descriptors: {sift_detect_and_compute_time - image_read_time}")
+    print(f"time for creating the dictionaries: {parameter_dictionary_time - sift_detect_and_compute_time}")
+    print(f"time for calling FlannBasedMatcher and knnMatch: {matches_calculated_time - parameter_dictionary_time}")
+    print(
+        f"time for calculating the homography matrix and mask: {homography_calculated_time - matches_calculated_time}")
 
 
-def orb_calculate(first_image,second_image,path_to_save_result,orb_create_matches=300,min_match_count = 10, flann_index_lsh= 6):
+def orb_calculate(first_image, second_image, path_to_save_result, orb_create_matches=300, min_match_count=10,
+                  flann_index_lsh=6):
     start_time = time.time()
 
     # Minimum number of matches required
@@ -114,8 +117,8 @@ def orb_calculate(first_image,second_image,path_to_save_result,orb_create_matche
 
     # Set FLANN parameters 
     FLANN_INDEX_LSH = flann_index_lsh
-    index_params = dict(algorithm = FLANN_INDEX_LSH, table_number = 6)
-    search_params = dict(checks = 50)
+    index_params = dict(algorithm=FLANN_INDEX_LSH, table_number=6)
+    search_params = dict(checks=50)
     parameter_dictionary_time = time.time()
 
     # Create the FLANN matcher and find matches (best two matches for each descriptor)
@@ -133,18 +136,17 @@ def orb_calculate(first_image,second_image,path_to_save_result,orb_create_matche
             m, n = match_pair
             if m.distance < 0.7 * n.distance:
                 good_matches.append(m)
-        else: 
-            skipped_matches+=1
-
+        else:
+            skipped_matches += 1
 
     # If enough matches are found, calculate the homography
     if len(good_matches) >= MIN_MATCH_COUNT:
         print(f'Found: {len(good_matches)} good matches out of {len(matches)} total matches')
         print(f'skipped: {skipped_matches} because there weren\'t two pairs')
-        
+
         # Get the keypoints from the good matches
-        src_pts = np.float32([ kp1[m.queryIdx].pt for m in good_matches ]).reshape(-1, 1, 2)
-        dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good_matches ]).reshape(-1, 1, 2)
+        src_pts = np.float32([kp1[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+        dst_pts = np.float32([kp2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
 
         # Calculate the homography matrix and mask
         M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
@@ -153,15 +155,15 @@ def orb_calculate(first_image,second_image,path_to_save_result,orb_create_matche
         homography_calculated_time = time.time()
         # Plot and display the results
         h, w = img1.shape[0:2]
-        pts = np.float32([[0, 0],[0, h-1],[w-1, h-1],[w-1, 0]]).reshape(-1, 1, 2) # get the corners
-        outline = cv2.perspectiveTransform(pts, M) # transform the corners of the first image onto the second
-        img2 = cv2.polylines(img2, [np.int32(outline)], True, 255, 20, cv2.LINE_AA) # draw the box
+        pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)  # get the corners
+        outline = cv2.perspectiveTransform(pts, M)  # transform the corners of the first image onto the second
+        img2 = cv2.polylines(img2, [np.int32(outline)], True, 255, 20, cv2.LINE_AA)  # draw the box
         draw_params = dict(
-                singlePointColor = None,
-                matchesMask = matchesMask,
-                flags = 2,
-                matchesThickness=20      # Adjust this value to change the line thickness between matched points
-                )
+            singlePointColor=None,
+            matchesMask=matchesMask,
+            flags=2,
+            matchesThickness=20  # Adjust this value to change the line thickness between matched points
+        )
         img3 = cv2.drawMatches(img1, kp1, img2, kp2, good_matches, None, **draw_params)
         plt.imshow(img3, 'gray')
         plt.savefig(path_to_save_result)
@@ -173,23 +175,30 @@ def orb_calculate(first_image,second_image,path_to_save_result,orb_create_matche
 
     end_time = time.time()
     elapsed_time = end_time - start_time
-    print(f"time for image reading: {image_read_time-start_time}")
-    print(f"time for detecting and computing ORB keypoints and descriptors: {orb_detect_and_compute_time-image_read_time}")
-    print(f"time for creating the dictionaries: {parameter_dictionary_time-orb_detect_and_compute_time}")
-    print(f"time for calling FlannBasedMatcher and knnMatch: {matches_calculated_time-parameter_dictionary_time}")
-    print(f"time for calculating the homography matrix and mask: {homography_calculated_time-matches_calculated_time}")
+    print(f"time for image reading: {image_read_time - start_time}")
+    print(
+        f"time for detecting and computing ORB keypoints and descriptors: {orb_detect_and_compute_time - image_read_time}")
+    print(f"time for creating the dictionaries: {parameter_dictionary_time - orb_detect_and_compute_time}")
+    print(f"time for calling FlannBasedMatcher and knnMatch: {matches_calculated_time - parameter_dictionary_time}")
+    print(
+        f"time for calculating the homography matrix and mask: {homography_calculated_time - matches_calculated_time}")
     return elapsed_time
 
-elapsed_100 = orb_calculate("../images/keyboard_better_rotated.jpg","../images/keyboard_better.jpg",'key_board_better_inverted_compare.png',orb_create_matches=100)
-elapsed_2000 = orb_calculate("../images/keyboard_better_rotated.jpg","../images/keyboard_better.jpg",'key_board_better_inverted_compare.png',orb_create_matches=2000)
-elapsed_4000 = orb_calculate("../images/keyboard_better_rotated.jpg","../images/keyboard_better.jpg",'key_board_better_inverted_compare.png',orb_create_matches=4000)
-elapsed_6000 = orb_calculate("../images/keyboard_better_rotated.jpg","../images/keyboard_better.jpg",'key_board_better_inverted_compare.png',orb_create_matches=6000)
-elapsed_8000 = orb_calculate("../images/keyboard_better_rotated.jpg","../images/keyboard_better.jpg",'key_board_better_inverted_compare.png',orb_create_matches=8000)
-print(f'100: {elapsed_100}')
-print(f'2000: {elapsed_2000}, increasing from 100 to 2000 was {elapsed_2000/elapsed_100}')
-print(f'4000: {elapsed_4000}, increasing from 2000 to 4000 was a {elapsed_4000/elapsed_2000} times increase')
-print(f'6000: {elapsed_6000}, increasing from 4000 to 6000 was a {elapsed_6000/elapsed_4000} times increase')
-print(f'8000: {elapsed_8000}, increasing from 6000 to 8000 was a {elapsed_8000/elapsed_6000} times increase')
 
+elapsed_100 = orb_calculate("../images/keyboard_better_rotated.jpg", "../images/keyboard_better.jpg",
+                            'key_board_better_inverted_compare.png', orb_create_matches=100)
+elapsed_2000 = orb_calculate("../images/keyboard_better_rotated.jpg", "../images/keyboard_better.jpg",
+                             'key_board_better_inverted_compare.png', orb_create_matches=2000)
+elapsed_4000 = orb_calculate("../images/keyboard_better_rotated.jpg", "../images/keyboard_better.jpg",
+                             'key_board_better_inverted_compare.png', orb_create_matches=4000)
+elapsed_6000 = orb_calculate("../images/keyboard_better_rotated.jpg", "../images/keyboard_better.jpg",
+                             'key_board_better_inverted_compare.png', orb_create_matches=6000)
+elapsed_8000 = orb_calculate("../images/keyboard_better_rotated.jpg", "../images/keyboard_better.jpg",
+                             'key_board_better_inverted_compare.png', orb_create_matches=8000)
+print(f'100: {elapsed_100}')
+print(f'2000: {elapsed_2000}, increasing from 100 to 2000 was {elapsed_2000 / elapsed_100}')
+print(f'4000: {elapsed_4000}, increasing from 2000 to 4000 was a {elapsed_4000 / elapsed_2000} times increase')
+print(f'6000: {elapsed_6000}, increasing from 4000 to 6000 was a {elapsed_6000 / elapsed_4000} times increase')
+print(f'8000: {elapsed_8000}, increasing from 6000 to 8000 was a {elapsed_8000 / elapsed_6000} times increase')
 
 # sift_calculate("../images/keyboard_better_rotated.jpg","../images/keyboard_better_rotated.jpg",'sift_function_test.png')
