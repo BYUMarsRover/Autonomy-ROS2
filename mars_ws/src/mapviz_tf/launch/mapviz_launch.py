4b9fbf887593d4d7c4bce68522273ca5f559acfc
wyomoike <wyomike2020@gmail.com>
2024-11-25 09:33:45 -0700

    Got dynamic launch arguments for location working!

--- mars_ws/src/mapviz_tf/launch/mapviz_launch.py
+++ mars_ws/src/mapviz_tf/launch/mapviz_launch.py
@@ -61,7 +61,7 @@ def generate_launch_description():
 
         # Declare launch arguments
         DeclareLaunchArgument('print_profile_data', default_value='false'),
-        # DeclareLaunchArgument('location', default_value=LaunchConfiguration('MAPVIZ_LOCATION', default='hanksville')),
+        DeclareLaunchArgument('location', default_value=LaunchConfiguration('MAPVIZ_LOCATION', default='hanksville')),
 
         LogInfo(msg=['Config file path: ', config_path]),
 
@@ -83,8 +83,33 @@ def generate_launch_description():
             name='initialize_origin',
             parameters=[{
                 'local_xy_frame': '/map',
-                'local_xy_origin': current_location,  # Pass location name directly
-                'local_xy_origins': manual_list  # Pass the resolved dictionary entry directly
+                'local_xy_origin': LaunchConfiguration('location'),   #DO THE THINGIMAJIG HERE
+                'local_xy_origins': """- name: byu
+  latitude: 40.2497218
+  longitude: -111.649276
+  altitude: 1376.0
+  heading: 0.0
+- name: rock_canyon
+  latitude: 41.267147
+  longitude: -111.632455
+  altitude: 0.0
+  heading: 0.0
+- name: hanksville
+  latitude: 38.406441
+  longitude: -110.791932
+  altitude: 1375.0
+  heading: 0.0
+- name: gravel_pit
+  latitude: 40.322243
+  longitude: -111.644278
+  altitude: 1500.0
+  heading: 0.0
+- name: little_moab
+  latitude: 40.057020
+  longitude: -112.012014
+  altitude: 1500.0
+  heading: 0.0
+"""
             }]
         ),
 
