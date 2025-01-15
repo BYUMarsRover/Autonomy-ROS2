8b8ec71a566c09a45e1864e5bb149385ecf51753
Daniel Webb <dbw2002@byu.edu>
2025-01-14 21:14:06 -0700

    FEAT: GPS and enable disable are functional

--- mars_ws/src/autonomy/autonomy/autonomy_gui.py
+++ mars_ws/src/autonomy/autonomy/autonomy_gui.py
@@ -1,3 +1,13 @@
+"""
+Autonomy GUI
+
+By: Daniel Webb and Gabe Slade - 2025
+
+Notes: 
+The QWidget Runs all the time and the ros node is spun at
+
+"""
+
 import rclpy
 from rclpy.node import Node
 
@@ -8,12 +18,12 @@ from PyQt5.QtCore import *
 from subprocess import Popen, PIPE
 import sys
 
+import threading
 import os
 
 from std_srvs.srv import SetBool
 from rover_msgs.srv import SetFloat32, AutonomyAbort, AutonomyWaypoint
-from rover_msgs.msg import AutonomyTaskInfo, RoverStateSingleton, RoverState, NavStatus, FiducialData, FiducialTransformArray, ObjectDetections
-#from ublox_read_2.msg import PositionVelocityTime #TODO: Uncomment this and get ublox_read_2 working, delete PositionVelocityTime from rover_msgs
+from rover_msgs.msg import AutonomyTaskInfo, PositionVelocityTime, RoverStateSingleton, RoverState, NavStatus, FiducialData, FiducialTransformArray, ObjectDetections
 
 class AutonomyGUI(Node, QWidget):
     def __init__(self):
@@ -26,11 +36,22 @@ class AutonomyGUI(Node, QWidget):
         uic.loadUi(os.path.expanduser('~') + '/mars_ws/src/autonomy/autonomy_gui.ui', self)
         self.show()  # Show the GUI
 
+        # Timer to periodically spin the ROS node
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.spin_ros)
+        self.timer.start(1)  # 1 second interval for spinning ROS
+
+
+        # Gui Buttons
         self.GNSSRadioButton.toggled.connect(self.update_leg_subselection)
         self.ArUcoRadioButton.toggled.connect(self.update_leg_subselection)
         self.ObjectRadioButton.toggled.connect(self.update_leg_subselection)
 
-        # Set initial page
+        self.EnableAutonomyButton.clicked.connect(self.enable_autonomy)
+        self.DisableAutonomyButton.clicked.connect(self.disable_autonomy)
+        self.AbortButton.clicked.connect(self.abort_autonomy)
+
+        # Set initial leg subselection options page to GNSS Page (0)
         self.legsubselectionStackedWidget.setCurrentIndex(0)
 
         #Initialize variables
@@ -48,42 +69,51 @@ class AutonomyGUI(Node, QWidget):
         # Publishers
 
         # Subscribers
-        #self.create_subscription(PositionVelocityTime, '/base/PosVelTime', self.base_GPS_info_callback, 10) #GPS info from base station
-        #self.create_subscription(PositionVelocityTime, '/rover/PosVelTime', self.rover_GPS_info_callback, 10) #GPS info from rover
-        self.create_subscription(RoverState, "/rover_status", self.rover_state_callback, 10) #Rover state (speed, direction, navigation state)
+        self.create_subscription(PositionVelocityTime, '/base/PosVelTime', self.base_GPS_info_callback, 10) #GPS info from base station
+        self.create_subscription(PositionVelocityTime, '/rover/PosVelTime', self.rover_GPS_info_callback, 10) #GPS info from rover
+        self.create_subscription(RoverState, '/rover_status', self.rover_state_callback, 10) #Rover state (speed, direction, navigation state)
         self.create_subscription(NavStatus, '/nav_status', self.rover_nav_status_callback, 10) #Autonomy State machine status
 
         # Services
 
         # Clients
-        # self.enable_autonomy_client = self.create_client(SetBool, '/autonomy/enable_autonomy')
+        self.enable_autonomy_client = self.create_client(SetBool, '/autonomy/enable_autonomy')
         # self.send_waypoint_client = self.create_client(AutonomyWaypoint, '/AU_waypoint_service')
-        # self.abort_autonomy_client = self.create_client(AutonomyAbort, '/autonomy/abort_autonomy')
+        self.abort_autonomy_client = self.create_client(AutonomyAbort, '/autonomy/abort_autonomy')
 
+    def spin_ros(self):
+        rclpy.spin_once(self)
 
-    #Callbacks for Subscribers
+    # Callbacks for Subscribers
     def base_GPS_info_callback(self, msg):
         self.base_GPS_info = msg
-        self.base_numSV = msg.numSV
+        self.base_numSV = msg.num_sv
         base_year = msg.year
         base_month = msg.month
         base_day = msg.day
         base_hour = msg.hour
         base_min = msg.min
         base_sec = msg.sec
-        self.base_date_time = f'Base Station Date: {base_month}/{base_day}/{base_year}/  Time: {base_hour}:{base_min}:{base_sec}'
-        return
-    
+        # Update Gui Fields
+        self.BaseSats.setText(f'Satellites: {self.base_numSV}')
+        self.BaseDate.setText(f'Date: {base_month}/{base_day}/{base_year}')
+        self.BaseTime.setText(f'Time: {base_hour}:{base_min}:{base_sec}')
+        # self.base_date_time = f'Base Station Date: {base_month}/{base_day}/{base_year}/  Time: {base_hour}:{base_min}:{base_sec}'
+
     def rover_GPS_info_callback(self, msg):
         self.rover_GPS_info = msg
-        self.rover_numSV = msg.numSV
+        self.rover_numSV = msg.num_sv
         rover_year = msg.year
         rover_month = msg.month
         rover_day = msg.day
         rover_hour = msg.hour
         rover_min = msg.min
         rover_sec = msg.sec
-        self.rover_date_time = f'Rover Date: {rover_month}/{rover_day}/{rover_year}/  Time: {rover_hour}:{rover_min}:{rover_sec}'
+        # Update Gui Fields
+        self.RoverSats.setText(f'Satellites: {self.rover_numSV}')
+        self.RoverDate.setText(f'Date: {rover_month}/{rover_day}/{rover_year}')
+        self.RoverTime.setText(f'Time: {rover_hour}:{rover_min}:{rover_sec}')
+        # self.rover_date_time = f'Rover Date: {rover_month}/{rover_day}/{rover_year}/  Time: {rover_hour}:{rover_min}:{rover_sec}'
         return
 
     def rover_state_callback(self, msg):
@@ -92,18 +122,18 @@ class AutonomyGUI(Node, QWidget):
         self.direction = msg.direction
         navigation_state = msg.navigation_state
         if navigation_state == 0:
-            self.navigation_state = 'AUTONOMOUS_STATE'
+            self.navigation_state = 'AUTONOMOUS'
         elif navigation_state == 1:
-            self.navigation_state = 'TELEOPERATION_STATE'
+            self.navigation_state = 'TELEOPERATION'
         elif navigation_state == 2:
-            self.navigation_state = 'ARRIVAL_STATE'
+            self.navigation_state = 'ARRIVAL'
         else:
-            self.navigation_state = 'UNKNOWN_STATE'
+            self.navigation_state = 'UNKNOWN'
         
         self.rover_state = f'Speed: {self.speed} m/s\nDirection: {self.direction} degrees\nNavigation State: {self.navigation_state}'
-
+        self.CurrentStateDisplay.setText(self.navigation_state)
         return
-    
+
     def rover_nav_status_callback(self, msg):
         self.rover_nav_status = msg
         self.state_machine_state = msg.state
@@ -113,7 +143,6 @@ class AutonomyGUI(Node, QWidget):
         else:
             self.autonomous_enable = 'Disabled'
         self.nav_status = f'State Machine State: {self.state_machine_state}\n State Machine: {self.autonomous_enable}'
-
         return
 
     # Callback functions for buttons
@@ -121,20 +150,21 @@ class AutonomyGUI(Node, QWidget):
         req = SetBool.Request()
         req.data = True
         future = self.enable_autonomy_client.call_async(req)
+        self.error_label.setText('Enabling Autonomy...')
         rclpy.spin_until_future_complete(self, future)
-        self.error_label.setText('Enabling Autonomy')
         
         if future.result().success:
-            self.error_label.setText('Task aborted. Manual mode turned on.')
+            self.error_label.setText('Autonomy Enabled')
         else:
-            self.error_label.setText('Failed to Abort Autonomy')
+            self.error_label.setText('Failed to Enable Autonomy')
+        return
 
     def disable_autonomy(self):
         req = SetBool.Request()
         req.data = False
         future = self.enable_autonomy_client.call_async(req)
+        self.error_label.setText('Disabling Autonomy...')
         rclpy.spin_until_future_complete(self, future)
-        self.error_label.setText('Disabling Autonomy')
         
         if future.result().success:
             self.error_label.setText('Autonomy Disabled')
@@ -174,6 +204,7 @@ class AutonomyGUI(Node, QWidget):
                 self.error_label.setText(f'Failed to Send Waypoint: {response.message}')
         else:
             self.error_label.setText('Service call failed or did not complete')
+        return
 
     def abort_autonomy(self):
         #logic for aborting autonomy task
@@ -206,7 +237,8 @@ class AutonomyGUI(Node, QWidget):
                 self.error_label.setText(f'Failed to Abort: {response.message}')
         else:
             self.error_label.setText('Service call failed or did not complete')
-   
+        return
+
     # Gui Functions
     def update_leg_subselection(self):
         if self.GNSSRadioButton.isChecked():
@@ -215,7 +247,10 @@ class AutonomyGUI(Node, QWidget):
             self.legsubselectionStackedWidget.setCurrentIndex(1)
         elif self.ObjectRadioButton.isChecked():
             self.legsubselectionStackedWidget.setCurrentIndex(2)
+        return
         
+def gui_ros_spin_thread(node):
+    rclpy.spin(node)
 
 def main(args=None):
     # Initialize ROS2
@@ -223,11 +258,24 @@ def main(args=None):
 
     Popen("pkill gst", shell=True, preexec_fn=os.setsid, stderr=PIPE)
     # Create QApplication
-    app = QApplication(sys.argv)
-    # Create GUI
-    gui = AutonomyGUI()
-    # Start GUI
-    app.exec_()
+    gui_QWidget = QApplication(sys.argv)
+
+    # Create ROS Gui
+    gui_ros = AutonomyGUI()
+
+    # ros_thread = threading.Thread(target=gui_ros_spin_thread, args=(gui_ros,), daemon=True)
+    # ros_thread.start() # Start gui ROS thread
+
+    # Run the Qt event loop
+    # try:
+    #     sys.exit(gui_QWidget.exec_())
+    # except KeyboardInterrupt:
+    #     pass
+    # finally:
+    #     rclpy.shutdown()
+    #     ros_thread.join()
+
+    gui_QWidget.exec_()
     rclpy.shutdown()
 
 if __name__ == '__main__':
