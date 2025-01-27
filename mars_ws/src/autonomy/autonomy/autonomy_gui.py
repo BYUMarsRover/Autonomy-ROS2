97b4fcf91e07adb6c9f5ad04b13db4be4145b3c4
grs45 <grs45@byu.edu>
2025-01-27 09:50:36 -0700

    WIP-Uncommented a log message in autonomy gui

--- mars_ws/src/autonomy/autonomy/autonomy_gui.py
+++ mars_ws/src/autonomy/autonomy/autonomy_gui.py
@@ -182,7 +182,6 @@ class AutonomyGUI(Node, QWidget):
         return
 
     def rover_nav_status_callback(self, msg): #State machine status (state, auto_enable)
-        self.get_logger().info(f'Nav Status: {msg}')
         self.rover_nav_status = msg
         if self.state_machine_state != None and self.state_machine_state != msg.state:
             self.prev_state_machine_state = self.state_machine_state
