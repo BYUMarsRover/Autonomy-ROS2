4199461ebf6fff8cdd8e3a34ee9914c14625bdf4
chlohilt <hilton.chloe.y@gmail.com>
2025-02-04 08:57:47 -0700

    successful image pipelie

--- mars_ws/src/keyboard_autonomy/keyboard_autonomy/feat2homography.py
+++ mars_ws/src/keyboard_autonomy/keyboard_autonomy/feat2homography.py
@@ -49,6 +49,18 @@ class Feat2HomographyNode(Node):
         self.bridge = CvBridge()
         self.image_prefix = "/home/marsrover/mars_ws/src/keyboard_autonomy/images/"
 
+        self.frame_rate = 10.0
+        timer_period = 1.0 / self.frame_rate
+
+        self.camera = cv2.VideoCapture(1)  # Open the default camera
+        
+        if not self.camera.isOpened():
+            self.get_logger().error("Failed to open camera!")
+            return
+        
+        self.timer = self.create_timer(timer_period, self.listener_callback)
+
+        self.camera_image = None
         self.get_logger().info("Feat2HomographyNode started")
 
 
@@ -96,11 +108,11 @@ class Feat2HomographyNode(Node):
 
         full_file_name = self.image_prefix + self.key_picture_file
         self.keyboard_img = cv2.imread(full_file_name)
-
+        self.get_logger().info("Key press call back setting keyboard img")
         response.success = False
         return response
 
-    def listener_callback(self, msg):
+    def listener_callback(self):
         '''
         Callback function for the "/image_raw" subscription.
         Uses the SIFT and FLANN algorithms to find the homography between the keyboard image and the
@@ -108,14 +120,28 @@ class Feat2HomographyNode(Node):
 
         :param msg: The Image message received from the Image topic.
         '''
+
+        ret, frame = self.camera.read()
+        if ret:
+            # Optionally, store the image in the object
+            self.camera_image = frame
+            self.get_logger().info("Image capture success.")
+        else:
+            self.get_logger().warn("Failed to capture image from the camera.")
+
         # Convert ROS Image to OpenCV
-        camera_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        # self.camera_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
 
         # SIFT and FLANN
         # Initiate SIFT detector and find the keypoints and descriptors
         sift = cv2.SIFT_create(nfeatures=500, nOctaveLayers=2, contrastThreshold=0.04, edgeThreshold=5)
+        if self.keyboard_img is None:
+            self.get_logger().info("keyboard img is none")
+            self.keyboard_img = cv2.imread("/home/marsrover/mars_ws/src/keyboard_autonomy/images/m_key_crop_light.png")
         kp1, des1 = sift.detectAndCompute(self.keyboard_img, None)
-        kp2, des2 = sift.detectAndCompute(camera_image, None)
+        if self.camera_image is None:
+            self.get_logger().info("camera img is none")
+        kp2, des2 = sift.detectAndCompute(self.camera_image, None)
 
         # Set FLANN parameters
         FLANN_INDEX_KDTREE = 0
@@ -143,29 +169,28 @@ class Feat2HomographyNode(Node):
 
             # Calculate the homography matrix using RANSAC
             M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+            matchesMask = mask.ravel().tolist()
 
+            # TODO: get rid of the drawn outline when this node is finished
+            h, w = self.keyboard_img.shape[0:2]
+            pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)  # get the corners
+            outline = cv2.perspectiveTransform(pts, M)  # transform the corners of the first image onto the second
+            img2 = cv2.polylines(self.camera_image, [np.int32(outline)], True, 255, 3, cv2.LINE_AA)  # draw the box
+
+            draw_params = dict(
+                singlePointColor=None,
+                matchesMask=matchesMask,
+                flags=2)
+            img3 = cv2.drawMatches(self.keyboard_img, kp1, img2, kp2, good_matches, None, **draw_params)
+            cv2.imwrite("matches.jpg", img3)
+            keyboard_homography = KeyboardHomography()
+            keyboard_homography.homography = M.flatten().tolist()
+            self.get_logger().info( M.flatten().tolist().toString())
+            self.publisher.publish(keyboard_homography)
         else:
             self.get_logger().warn("Insufficient # of matches found - %d/%d" % (len(good_matches), MIN_MATCH_COUNT))
 
-        matchesMask = mask.ravel().tolist()
-
-        # TODO: get rid of the drawn outline when this node is finished
-        h, w = self.keyboard_img.shape[0:2]
-        pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)  # get the corners
-        outline = cv2.perspectiveTransform(pts, M)  # transform the corners of the first image onto the second
-        img2 = cv2.polylines(camera_image, [np.int32(outline)], True, 255, 3, cv2.LINE_AA)  # draw the box
-
-        draw_params = dict(
-            singlePointColor=None,
-            matchesMask=matchesMask,
-            flags=2)
-        img3 = cv2.drawMatches(self.keyboard_img, kp1, img2, kp2, good_matches, None, **draw_params)
-        cv2.imwrite("matches.jpg", img3)
-        keyboard_homography = KeyboardHomography()
-        keyboard_homography.homography = M.flatten().tolist()
-
-        self.publisher.publish(keyboard_homography)
-
+        
 
 def main(args=None):
     rclpy.init(args=args)
