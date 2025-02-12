90742c8fdcd437697ccdde222378439e27cd9d48
byurover <byuawesomerover@gmail.com>
2025-02-12 12:25:39 -0700

    Estimation new launch with map yaw coming from filter madgewick. No UKF running.

--- mars_ws/src/odometry/odometry/rover_state_singleton_creator_new.py
+++ mars_ws/src/odometry/odometry/rover_state_singleton_creator_new.py
@@ -8,16 +8,72 @@ from rclpy.node import Node
 import numpy as np
 np.float = float  # Temporary alias for compatibility
 
-from tf_transformations import euler_from_quaternion
+# from tf_transformations import euler_from_quaternion, quaternion_multiply
+
+from tf2_ros import TransformException
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
 
 from odometry.transform_tools import quat2R, R2quat
 
+from geometry_msgs.msg import TransformStamped, Vector3Stamped
 from nav_msgs.msg import Odometry
 from rover_msgs.msg import RoverStateSingleton
 from sensor_msgs.msg import NavSatFix, Imu
 import numpy as np
 
 np.float = float # Bug fix hack
+
+def euler_from_quaternion(quat):
+    """Convert a quaternion into euler angles (roll, pitch, yaw).
+    
+    quat: [x, y, z, w]
+    returns: (roll, pitch, yaw) in radians
+    """
+
+    x, y, z, w = quat
+    # Roll (x-axis rotation)
+    sinr_cosp = 2 * (w * x + y * z)
+    cosr_cosp = 1 - 2 * (x * x + y * y)
+    roll = math.atan2(sinr_cosp, cosr_cosp)
+
+    # Pitch (y-axis rotation)
+    sinp = 2 * (w * y - z * x)
+    if abs(sinp) >= 1:
+        pitch = math.copysign(math.pi / 2, sinp)  # Use 90 degrees if out of range
+    else:
+        pitch = math.asin(sinp)
+
+    # Yaw (z-axis rotation)
+    siny_cosp = 2 * (w * z + x * y)
+    cosy_cosp = 1 - 2 * (y * y + z * z)
+    yaw = math.atan2(siny_cosp, cosy_cosp)
+
+    return roll, pitch, yaw
+
+def quat_to_list(quat):
+    return [quat.x, quat.y, quat.z, quat.w]
+
+def list_to_quat(list):
+    quat = Imu().orientation
+    quat.x = list[0]
+    quat.y = list[1]
+    quat.z = list[2]
+    quat.w = list[3]
+    return quat
+
+def quaternion_multiply(q1, q2):
+    """Multiply two quaternions."""
+    q1 = quat_to_list(q1)
+    q2 = quat_to_list(q2)
+    w1, x1, y1, z1 = q1
+    w2, x2, y2, z2 = q2
+    return list_to_quat(np.array([
+        w1*w2 - x1*x2 - y1*y2 - z1*z2,
+        w1*x2 + x1*w2 + y1*z2 - z1*y2,
+        w1*y2 - x1*z2 + y1*w2 + z1*x2,
+        w1*z2 + x1*y2 - y1*x2 + z1*w2
+    ]))
 
 class RoverStateSingletonCreator(Node):
     """
@@ -26,16 +82,26 @@ class RoverStateSingletonCreator(Node):
     def __init__(self):
         super().__init__('rover_state_singleton_creator')
 
+        # TF listner
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+
         # Subscribers
-        self.zed_orientation_subscription = self.create_subscription(Imu, "/zed/imu/data", self.convert_map, 10)  # Subscribes to the ZED orientation data
+        # USED THIS SUBSCRIBER when trusting the zed orientation
+        # self.zed_orientation_subscription = self.create_subscription(Odometry, "/zed/odom", self.convert_map, 10)  # Subscribes to the ZED orientation data
+        # USED THIS SUBSCRIBER WHEN trusting the filter madgewick
+        self.imu_filter_sub = self.create_subscription(Vector3Stamped, "/imu/rpy/filtered", self.set_map_yaw, 10)
 
         # TODO: Chekc the filtered GPS subscription
-        self.filtered_gps_subscription = self.create_subscription(NavSatFix, "/gps/filtered", self.convert_filtered_gps, 10)  # Subscribes to the filtered GPS data from the UKF output
+        self.unfiltered_gps_subscription = self.create_subscription(NavSatFix, "/fix", self.convert_unfiltered_gps, 10)  # Subscribes to the filtered GPS data from the UKF output
         # self.gps_subscription = self.create_subscription(NavSatFix, "/ins/lla", self.convert_gps, 10) # Subscribes to unfiltered GPS data
         self.gps_subscription = self.create_subscription(NavSatFix, "/zed/global", self.convert_gps, 10) # Subscribes to zed filtered GPS data
 
         # Publishers
         self.singleton_publisher = self.create_publisher(RoverStateSingleton, '/odometry/rover_state_singleton', 10) # Publishes the singleton message
+
+        # timer
+        self.transform_lookup = self.create_timer(10.0, self.transform_check)
 
         self.map_roll = 0.0
         self.map_pitch = 0.0
@@ -70,13 +136,37 @@ class RoverStateSingletonCreator(Node):
         self.odom_yaw_dot = 0.0
 
         self.gps = NavSatFix()
-        self.filter_gps = NavSatFix()
+        self.raw_gps = NavSatFix()
 
+        self.t = TransformStamped()
+
+    def transform_check(self):
+        try:
+            self.t = self.tf_buffer.lookup_transform(
+            'odom',
+            'map',
+            rclpy.time.Time())
+        except TransformException as ex:
+            self.get_logger().info(
+                f'Could not transform map to odom: {ex}')
+            return
+    
+
+    def set_map_yaw(self, msg):
+        #Take the euler angle from the madgewick filter directly and set the yaw
+
+        self.map_yaw = msg.vector.z * (180.0 / math.pi)
+
+        self.publish_message()
+    
+    
     def convert_map(self, message):
+
         """
         Converts orientation from quaternions to euler (converts them into human readable roll, pitch, yaw) angles and sets the map variables
         """
-        orientation_q = message.orientation  # Extracts the orientation data from the message
+        
+        orientation_q = quaternion_multiply(self.t.transform.rotation, message.pose.pose.orientation)  # Extracts the orientation data from the message
         # orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]  # Extracts quaternion data
 
         # R = quat2R(orientation_list)
@@ -112,7 +202,8 @@ class RoverStateSingletonCreator(Node):
         # self.map_roll = euler[0] * 180/math.pi
         # self.map_pitch = euler[1] * 180/math.pi
         # self.map_yaw = euler[2] * 180/math.pi 
-        self.map_yaw = (yaw * 180/math.pi ) - 65
+        self.map_yaw = (yaw * 180/math.pi )
+        self.get_logger().info(f"MAP YAW: {self.map_yaw}")
 
         if self.map_yaw < -180:
             self.map_yaw += 360
@@ -149,11 +240,11 @@ class RoverStateSingletonCreator(Node):
 
 
 
-    def convert_filtered_gps(self, message):
+    def convert_unfiltered_gps(self, message):
         """
-        Sets the filter GPS variable
+        Sets the unfilter GPS variable
         """
-        self.filter_gps = message
+        self.raw_gps = message
         self.publish_message()
 
     def convert_gps(self, message):
@@ -193,7 +284,7 @@ class RoverStateSingletonCreator(Node):
             odom_pitch_dot = self.odom_pitch_dot,
             odom_yaw_dot = self.odom_yaw_dot,
             gps = self.gps,
-            filter_gps = self.filter_gps
+            filter_gps = self.raw_gps
         )
         self.singleton_publisher.publish(message)
 
