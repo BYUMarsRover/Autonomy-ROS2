03482a10907e2dba53693e9ff27095ed7f96f2a5
Nelson Durrant <snelsondurrant@gmail.com>
2025-02-19 16:44:46 -0700

    Updates

--- mars_ws/scripts/startup.sh
+++ mars_ws/scripts/startup.sh
@@ -1,7 +1,7 @@
-tmux new-session -d -s autonomy_ros2 -n "mobility"
-tmux send-keys -t autonomy_ros2:mobility.0 "date" Enter
+# tmux new-session -d -s autonomy_ros2 -n "mobility"
+# tmux send-keys -t autonomy_ros2:mobility.0 "date" Enter
 # Start the main process (replace this with your actual process)
 
-/usr/sbin/sshd -D
+sudo /usr/sbin/sshd -D
 
 exec "$@"
