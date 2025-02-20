a274cfbcc6c55cb0591bdcb7e019833864b4e31b
Nelson Durrant <snelsondurrant@gmail.com>
2025-02-19 23:16:56 -0700

    Quick fix

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Created by Nelson Durrant, Sep 2024
 #
-# Simply pulls all avaliable updates
+# Simply pulls all available updates
 
 # Docker updates
 docker pull byuawesomerover/autonomy_ros2:latest
