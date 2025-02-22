8b31adc3ce6ec2127fb8e732142e7500580f5ab6
deathstar <marsrover@deathstar>
2025-02-22 12:21:29 -0700

    reverted to previous local nav strategy

--- mars_ws/src/mobility/mobility/aruco_autopilot_manager.py
+++ mars_ws/src/mobility/mobility/aruco_autopilot_manager.py
@@ -79,9 +79,9 @@ class ArucoAutopilotManager(Node):
         # This was used to make it so the rover would either only turn or only drive forward
         # This is not needed anymore because the PID controllers can handle it
         # TODO: We just need to tune the gains for the controller for good performance
-        # limit = 8 / 180 * np.pi
-        # if abs(self.angle_to_target) > limit:
-        #     self.distance = 0
+        limit = 8 / 180 * np.pi
+        if abs(self.angle_to_target) > limit:
+            self.distance = 0
 
         lin_vel = self.linear_controller.update_with_error(self.distance)
         angular_vel = self.angular_controller.update_with_error(self.angle_to_target)
