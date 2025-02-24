1110abeb1ade571259653f15225b6fc58ed3ad4d
Nelson Durrant <snelsondurrant@gmail.com>
2025-02-24 08:44:46 -0700

    Removed sshpass, fixed tabs, zed session color

--- launch.sh
+++ launch.sh
@@ -22,25 +22,43 @@ ROVER_IP_ADDRESS=192.168.1.120
 DOCKER_SSH_PORT=2233
 
 # Check for an SSH connection to the rover's Docker container
-if ! sshpass -p "marsrover" ssh marsrover@$ROVER_IP_ADDRESS -p $DOCKER_SSH_PORT "echo" &> /dev/null
+if ! ssh marsrover@$ROVER_IP_ADDRESS -p $DOCKER_SSH_PORT "echo" &> /dev/null
 then
     printError "No available SSH connection to the rover's Docker container"
     echo "Here's some debugging suggestions:"
     echo "  - Ensure the rover is powered on"
     echo "  - Ensure the rover is connected with a static IP address"
-    echo "  - Ensure the rover's computer has a green light"
-    echo "  - Ensure the rocket antennas are connected"
     echo "  - Ensure the rover's Docker container is running"
 
     exit
 fi
 
 # Check if tmux is running on the rover's Docker container
-if ! sshpass -p "marsrover" ssh marsrover@$ROVER_IP_ADDRESS -p $DOCKER_SSH_PORT "tmux has-session -t rover_runtime" &> /dev/null
+if ! ssh marsrover@$ROVER_IP_ADDRESS -p $DOCKER_SSH_PORT "tmux has-session -t rover_runtime" &> /dev/null
 then
     printError "No tmux session found in the rover's Docker container"
     echo "Here's some debugging suggestions:"
     echo "  - Ensure the rover's Docker container is running the 'rover_runtime' tmux session"
+
+    exit
+fi
+
+# Check that only one window is open in the 'rover_runtime' tmux session
+if [ $(ssh marsrover@$ROVER_IP_ADDRESS -p $DOCKER_SSH_PORT "tmux list-windows -t rover_runtime | wc -l") -ne 1 ]
+then
+    printWarning "Multiple windows found in the 'rover_runtime' tmux session"
+    echo "Simply entering the current tmux session for cleanup..."
+    ssh -t -X marsrover@$ROVER_IP_ADDRESS -p $DOCKER_SSH_PORT 'tmux attach -t rover_runtime'
+
+    exit
+fi
+
+# Check that only one pane is open in the 'rover_runtime' tmux session
+if [ $(ssh marsrover@$ROVER_IP_ADDRESS -p $DOCKER_SSH_PORT "tmux list-panes -t rover_runtime | wc -l") -ne 1 ]
+then
+    printWarning "Multiple panes found in the 'rover_runtime' tmux session"
+    echo "Simply entering the current tmux session for cleanup..."
+    ssh -t -X marsrover@$ROVER_IP_ADDRESS -p $DOCKER_SSH_PORT 'tmux attach -t rover_runtime'
 
     exit
 fi
@@ -50,7 +68,7 @@ case "$1" in
     "autonomy")
         printInfo "Setting up the autonomy task..."
         # Send tmux commands to the rover's Docker container over SSH
-        sshpass -p "marsrover" ssh marsrover@$ROVER_IP_ADDRESS -p $DOCKER_SSH_PORT "\
+        ssh marsrover@$ROVER_IP_ADDRESS -p $DOCKER_SSH_PORT "\
             tmux split-window -h -t rover_runtime; \
             tmux select-pane -t rover_runtime.1; \
             tmux send-keys -t rover_runtime.1 'ros2 launch start rover_task_autonomy_new_launch.py'" # NO ENTER 
@@ -65,10 +83,10 @@ case "$1" in
         printWarning "Not implemented yet"
         ;;
     *)
-        printWarning "No task specified, simply entering the current tmux session"
-        printWarning "Specify a task using 'bash launch.sh <task>' (ex. 'bash launch.sh autonomy')"
+        printWarning "No task specified, simply entering the current tmux session..."
+        echo "Specify a task using 'bash launch.sh <task>' (ex. 'bash launch.sh autonomy')"
         ;;
 esac
 
 # Attach to the 'rover_runtime' tmux session
-sshpass -p "marsrover" ssh -t -X marsrover@$ROVER_IP_ADDRESS -p $DOCKER_SSH_PORT 'tmux attach -t rover_runtime'
+ssh -t -X marsrover@$ROVER_IP_ADDRESS -p $DOCKER_SSH_PORT 'tmux attach -t rover_runtime'
