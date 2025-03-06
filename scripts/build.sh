4e205acf1e196df58046328e0fabbbe2f3d817a0
Braden Meyers <bjz-github@spwiz.com>
2025-03-05 18:26:58 -0700

    build and sync git updated

---
+++ scripts/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "This script will build the Autonomy-ROS2 project."
+echo "You have the option to perform a clean build, which will remove the build, install, and log directories before building."
+
+read -p "Do you want to perform a clean build? (y/n): " clean_build
+
+if [ "$clean_build" == "y" ]; then
+    echo "Performing a clean build..."
+    docker exec -it autonomy_ros2 bash -c "rm -rf /home/marsrover/mars_ws/build /home/marsrover/mars_ws/install /home/marsrover/mars_ws/log"
+fi
+
+echo "Building the project..."
+docker exec -it autonomy_ros2 bash -c "cd /home/marsrover/mars_ws && colcon build"
+
+echo "Build process completed."
--- sync_git.sh
+++ sync_git.sh
@@ -4,21 +4,22 @@
 # Pulls the base station git branch and changes onto the rover
 
 function printInfo {
- 	# print blue
- 	echo -e "\033[0m\033[36m[INFO] $1\033[0m"
+     # print blue
+     echo -e "\033[0m\033[36m[INFO] $1\033[0m"
 }
 
 function printWarning {
-  	# print yellow
-  	echo -e "\033[0m\033[33m[WARNING] $1\033[0m"
+      # print yellow
+      echo -e "\033[0m\033[33m[WARNING] $1\033[0m"
 }
 
 function printError {
-  	# print red
-  	echo -e "\033[0m\033[31m[ERROR] $1\033[0m"
+      # print red
+      echo -e "\033[0m\033[31m[ERROR] $1\033[0m"
 }
 
 ROVER_IP_ADDRESS=192.168.1.120
+# DOCKER_SSH_PORT=2233
 
 # Get the current branch name
 current_branch=$(git branch --show-current)
@@ -35,16 +36,19 @@ then
     exit
 fi
 
+# Get the remote name from the first argument, default to 'base'
+remote=${1:-base}
+
 # Send tmux commands to the rover over SSH
 printInfo "Setting up the sync_git tmux session..."
 ssh marsrover@$ROVER_IP_ADDRESS "tmux new-session -d -s sync_git; \
-    tmux set-option -g default-terminal "screen-256color"; \
+    tmux set-option -g default-terminal \"screen-256color\"; \
     tmux set -g mouse on; \
     tmux send-keys -t sync_git.0 'clear' Enter; \
-    tmux send-keys -t sync_git.0 'cd ~/marsrover/mars_ws' Enter; \
+    tmux send-keys -t sync_git.0 'cd ~/Autonomy-ROS2' Enter; \
     tmux send-keys -t sync_git.0 'git checkout $current_branch' Enter; \
-    tmux send-keys -t sync_git.0 'git pull base $current_branch' Enter; \
-    tmux send-keys -t sync_git.0 'colcon build'" # NO ENTER
+    tmux send-keys -t sync_git.0 'git pull $remote $current_branch' Enter; \
+    tmux send-keys -t sync_git.0 'bash scripts/build.sh'" # NO ENTER
 
 # Attach to the 'sync_git' tmux session to view the output
 ssh -t -X marsrover@$ROVER_IP_ADDRESS "tmux attach -t sync_git"
