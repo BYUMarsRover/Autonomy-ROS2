4230956f35e19870442a7891947eac40dfe9427b
deathstar <marsrover@deathstar>
2025-05-07 23:10:26 -0600

    changed the entrypoint script to work for the new laptop

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -7,30 +7,32 @@
 
 # Are we running on Jetson Orin architecture (the rover)?
 
-if [ "$(uname -m)" == "aarch64" ]; then
-    # Start a new 'rover_runtime' tmux session
-    tmux new-session -d -s rover_runtime
+# if [ "$(uname -m)" == "aarch64" ]; then
 
-    # Send 'clear' command to the first window
-    tmux send-keys -t rover_runtime:0 "clear" Enter
-    # Full color and mouse options
-    tmux set-option -g default-terminal "screen-256color"
-    tmux set -g mouse on
-    # Create a new window within the 'rover_runtime' session and name it 'fastdds_server'
-    tmux new-window -t rover_runtime -n fastdds_server
+# Changed for the new laptop!
+# Start a new 'rover_runtime' tmux session
+tmux new-session -d -s rover_runtime
 
-    # Send the 'fastdds discovery' command to the 'fastdds_server' window
-    tmux send-keys -t rover_runtime:fastdds_server "fastdds discovery --server-id 0" Enter
+# Send 'clear' command to the first window
+tmux send-keys -t rover_runtime:0 "clear" Enter
+# Full color and mouse options
+tmux set-option -g default-terminal "screen-256color"
+tmux set -g mouse on
+# Create a new window within the 'rover_runtime' session and name it 'fastdds_server'
+tmux new-window -t rover_runtime -n fastdds_server
 
-    #let the discovery server startup before adding nodes
-    sleep 3
+# Send the 'fastdds discovery' command to the 'fastdds_server' window
+tmux send-keys -t rover_runtime:fastdds_server "fastdds discovery --server-id 0" Enter
 
-    # Launch ROS 2 nodes on system startup
+#let the discovery server startup before adding nodes
+sleep 3
 
-    tmux send-keys -t rover_runtime:0.0 "export ROS_DISCOVERY_SERVER=127.0.0.1:11811" Enter
-    tmux send-keys -t rover_runtime:0.0 "ros2 launch start rover_usb_devices_launch.py"
+# Launch ROS 2 nodes on system startup
 
-fi
+tmux send-keys -t rover_runtime:0.0 "export ROS_DISCOVERY_SERVER=127.0.0.1:11811" Enter
+tmux send-keys -t rover_runtime:0.0 "ros2 launch start rover_usb_devices_launch.py"
+
+# fi
 
 # Start the SSH daemon in the Docker container
 sudo /usr/sbin/sshd -D
