d728c55367344a3691de1b84fadc6193138e46b2
byurover <byuawesomerover@gmail.com>
2025-05-29 22:53:11 -0600

    new camera scripts

---
+++ scripts/camera/rover-launch-bird.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./rover-launch-single-camera.sh -a 192.168.1.111 -q Low -c 0 -d birdCam
---
+++ scripts/camera/rover-launch-probe.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./rover-launch-single-camera.sh -a 192.168.1.111 -q Low -c 3 -d scienceLegCam
